33f25fcd58ac52ed674f4255bf76793d90752bd4
Alexander Korus <alexander.korus@ffuf.de>
2019-06-15 19:22:54 +0200

    Add InsetTextField

---
+++ Source/UI/InsetTextField.swift
@@ -0,0 +1,24 @@
+//
+//  InsetTextField.swift
+//  Relieve
+//
+//  Created by Alexander Korus on 15.06.19.
+//
+import Foundation
+
+class TextField: UITextField {
+    
+    let padding = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+    
+    override open func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+    override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+}
