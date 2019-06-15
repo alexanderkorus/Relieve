20b2ccd99217136e39ab9141ccb563880adf14b0
Alexander Korus <alexander.korus@ffuf.de>
2019-06-15 22:15:33 +0200

    Add decodable extension

---
+++ Source/Extensions/Codable+Extensions.swift
@@ -0,0 +1,27 @@
+//
+//  Codable+Extensions.swift
+//  Relieve
+//
+//  Created by Alexander Korus on 15.06.19.
+//
+
+import Foundation
+
+extension Encodable {
+    
+    
+    
+}
+
+extension Decodable {
+    
+    static func from<T: Decodable>(data: Data) -> T? {
+        let decoder = JSONDecoder()
+        do {
+            return try decoder.decode(T.self, from: data)
+        } catch let error {
+            print("Parsing Error in: \(T.self) with message: \(error.localizedDescription)")
+            return nil
+        }
+    }
+}
