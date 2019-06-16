ac7025f944b445f067f22dd239fd136707d3a261
Alexander Korus <alexander.korus@ffuf.de>
2019-06-16 14:40:07 +0200

    Adjust uicolor extension

--- Source/Extensions/UIColor+Extensions.swift
+++ Source/Extensions/UIColor+Extensions.swift
@@ -14,30 +14,29 @@ public extension UIColor {
     which means it returns nil if you don't specify a color in the correct format. It should be a # symbol, followed
     by red, green, blue and alpha in hex format, for a total of nine characters.
     */
-    convenience init?(hex: String) {
-        let r, g, b, a: CGFloat
+    convenience init(hexValue: String) {
+        var hexValue: String = hexValue.uppercased().trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
         
-        if hex.hasPrefix("#") {
-            let start = hex.index(hex.startIndex, offsetBy: 1)
-            let hexColor = String(hex[start...])
-            
-            if hexColor.count == 8 {
-                let scanner = Scanner(string: hexColor)
-                var hexNumber: UInt64 = 0
-                
-                if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    a = CGFloat(hexNumber & 0x000000ff) / 255
-                    
-                    self.init(red: r, green: g, blue: b, alpha: a)
-                    return
-                }
-            }
+        if hexValue.hasPrefix("#") {
+            hexValue.remove(at: hexValue.startIndex)
         }
         
-        return nil
+        if hexValue.count != 6 {
+            self.init(red: 255/255,
+                      green: 255/255,
+                      blue: 255/255,
+                      alpha: 1.0
+            )
+        }
+        
+        var rgbValue: UInt32 = 0
+        Scanner(string: hexValue).scanHexInt32(&rgbValue)
+        
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: 1.0
+        )
     }
     
     convenience init(r: UInt8, g: UInt8, b: UInt8, a: CGFloat = 1.0) {
